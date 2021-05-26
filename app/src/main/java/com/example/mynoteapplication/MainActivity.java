9c574c71477753aecddef7ffc784c5c01e8f4a7c
Katherine23-z <eszaharova23@yandex.ru>
2021-05-26 22:24:39 +0300

    Lesson6 fragments

--- app/src/main/java/com/example/mynoteapplication/MainActivity.java
+++ app/src/main/java/com/example/mynoteapplication/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.mynoteapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,59 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
 
-    }
-
-    private void initView() {
-        initToolbar();
-    }
-
-    private void initToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_add:
-                Toast toastAdd = Toast.makeText(getApplicationContext(), "Add note", Toast.LENGTH_SHORT);
-                toastAdd.show();
-                return true;
-            case R.id.action_delete:
-                Toast toastDel = Toast.makeText(getApplicationContext(), "Delete note", Toast.LENGTH_SHORT);
-                toastDel.show();
-                return true;
-            case R.id.action_main:
-                Toast toastMain = Toast.makeText(getApplicationContext(), "Main", Toast.LENGTH_SHORT);
-                toastMain.show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        MenuItem search = menu.findItem(R.id.action_search);
-        SearchView searchText = (SearchView) search.getActionView();
-        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return true;
-            }
-        });
-
-
-        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/example/mynoteapplication/NoteContentActivity.java
+++ app/src/main/java/com/example/mynoteapplication/NoteContentActivity.java
@@ -1,15 +1,9 @@
 package com.example.mynoteapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 public class NoteContentActivity extends AppCompatActivity {
 
@@ -17,7 +11,6 @@ public class NoteContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_content);
-        initView();
 
         if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
             finish();
@@ -32,35 +25,5 @@ public class NoteContentActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, content)
                     .commit();
         }
-    }
-
-    private void initView() {
-        initToolbar();
-    }
-
-    private void initToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_attach:
-                Toast.makeText(getApplicationContext(), "Attach file", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_favourite:
-                Toast.makeText(getApplicationContext(), "Add to favourite", Toast.LENGTH_SHORT).show();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.content, menu);
-        return super.onCreateOptionsMenu(menu);
     }
 }
