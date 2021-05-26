2141e82811cdb99699a2694d6859f9d77e052322
Katherine23-z <eszaharova23@yandex.ru>
2021-05-26 22:21:43 +0300

    Revert "Lesson 7"
    This reverts commit bdd43e7a

--- app/src/main/java/com/example/mynoteapplication/MainActivity.java
+++ app/src/main/java/com/example/mynoteapplication/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.mynoteapplication;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,39 +21,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        Toolbar toolbar = initToolbar();
-        initDrawer(toolbar);
+        initToolbar();
     }
 
-    private void initDrawer(Toolbar toolbar) {
-        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(item -> {
-            int id = item.getItemId();
-            switch (id) {
-                case R.id.action_settings:
-                    Toast.makeText(MainActivity.this, "Open Settings", Toast.LENGTH_SHORT).show();
-                    //TODO addFragment(new SettingsFragment())
-                case R.id.action_share:
-                    Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
-                case R.id.action_favourite:
-                    Toast.makeText(MainActivity.this, "Add to favourite", Toast.LENGTH_SHORT).show();
-            } return false;
-        });
-
-
-
-    }
-
-    private Toolbar initToolbar() {
+    private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        return toolbar;
     }
 
     @Override
